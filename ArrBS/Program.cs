c28616451c36f78efd8b07ada7aa5c4f107b74fd
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-19 13:10:51 +0100

    Add project files.

---
+++ ArrBS/Program.cs
@@ -0,0 +1,36 @@
+﻿using Ordinamento;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ArrBS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimensioni = { 1, 20, 50, 100, 500, 1000,50000,60000};
+            using (StreamWriter sw = new StreamWriter("stat.csv", false, Encoding.UTF8))
+            {
+                sw.WriteLine("Algoritmo,Dimensione,Tipo");
+                foreach(int dim in dimensioni)
+                {
+                    int[] numeri = new int[dim];
+                    Random r = new Random(); 
+                    for(int i = 0; i < dim; i++)
+                        numeri[i] = r.Next();
+                    Stopwatch s = new Stopwatch();
+                    s.Start();
+                    BubbleSort.Sort(numeri);
+                    s.Stop();
+                    long elapsed = s.ElapsedMilliseconds;
+                    sw.WriteLine($"BubbleSort;{dim};{elapsed}");
+                    Console.WriteLine($"BubbleSort;{dim};{elapsed}");
+                }
+                sw.Flush();
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ordinamento
+{
+    public class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            int x, y;
+            int temp;
+            for (x=0;x< array.Length; x++)
+            {
+                for(y = 0; y < array.Length-1; y++)
+                    if(array[y] > array[y + 1])
+                    {
+                        temp = array[y];
+                        array[y] = array[y + 1];
+                        array[y + 1] = temp;
+                           
+                    }
+            }
+        }
+    }
+}
